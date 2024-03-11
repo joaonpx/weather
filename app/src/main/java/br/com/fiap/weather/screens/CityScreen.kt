31605d98e167d63acefe668e505db33b33ff7868
Npx <cheyprivado@gmail.com>
2024-03-11 00:11:57 -0300

    Remover imports que não foram utilizados

--- app/src/main/java/br/com/fiap/weather/screens/CityScreen.kt
+++ app/src/main/java/br/com/fiap/weather/screens/CityScreen.kt
@@ -2,10 +2,6 @@ package br.com.fiap.weather.screens
 
 import android.util.Log
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
